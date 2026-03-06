c59f5292ab29b74f36ab4802ac5e540c067a82d3
Roshan Pandey <rpandox@gmail.com>
2026-03-06 20:52:24 +0545

    feat(cloudflare): add cloudflare to the repo

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        pathname: '/7.x/**',
+      },
+    ],
   },
 }
 
